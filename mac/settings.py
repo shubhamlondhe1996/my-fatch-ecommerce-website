96a7684b30a342899c43ee2923b1ff13de878190
gautamk1512 <77240480+gautamk1512@users.noreply.github.com>
2021-06-03 16:01:52 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = 'mac.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config
-
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
